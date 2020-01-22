fc33d9dfca9bf1dbe7c279baa31d66b912a33546
Robert <qingdong.cqd@alibaba-inc.com>
2020-01-22 20:34:21 +0800

    feat: GLSurfaceView替换掉默认布局，同时重写GLSurfaceView.Renderer

--- Lesson1/app/src/main/java/com/example/Lesson1/MainActivity.java
+++ Lesson1/app/src/main/java/com/example/Lesson1/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 public class MainActivity extends AppCompatActivity {
     private GLSurfaceView mGlSurfaceView = null;
 
@@ -13,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         mGlSurfaceView = new GLSurfaceView(this);
+
+        mGlSurfaceView.setEGLContextClientVersion(2);
+        mGlSurfaceView.setRenderer(new GLRender1());
+
         setContentView(mGlSurfaceView);
     }
 }
