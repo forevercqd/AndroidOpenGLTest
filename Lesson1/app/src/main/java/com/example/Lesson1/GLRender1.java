cf7c05431a2eef633f9fa48f070cab65c89b1fd9
Robert <qingdong.cqd@alibaba-inc.com>
2020-01-22 22:17:51 +0800

    feat: 增加画线操作;

--- Lesson1/app/src/main/java/com/example/Lesson1/GLRender1.java
+++ Lesson1/app/src/main/java/com/example/Lesson1/GLRender1.java
@@ -1,24 +1,98 @@
 package com.example.Lesson1;
 
+import android.content.Context;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 
 public class GLRender1 implements GLSurfaceView.Renderer {
+    private static final int BYTES_PER_FLOAT = 4;
+    private final Context mContext;
+    private FloatBuffer vertexData;
+    private static final int POSITION_COMOPNENT_COUNT = 2;
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int STRIDE = (POSITION_COMOPNENT_COUNT+COLOR_COMPONENT_COUNT)*BYTES_PER_FLOAT;
+
+    private final String A_POSITION = "a_Position";
+    private final String A_COLOR = "a_Color";
+
+
+    private int program;
+    float[] tableVertices = {
+            //Order of coordinates:X,Y,R,G,B
+
+
+            //Triangle Fan
+            0f,     0f,     1.0f,1.0f,1.0f,
+            -0.5f,  -0.5f,  0.7f,0.7f,0.7f,
+            0.5f,   -0.5f,  0.7f,0.7f,0.7f,
+            0.5f,   0.5f,   0.7f,0.7f,0.7f,
+            -0.5f,  0.5f,   0.7f,0.7f,0.7f,
+            -0.5f,  -0.5f,  0.7f,0.7f,0.7f,
+
+            -0.5f,  0f,     1.0f,0.0f,0.0f,
+            0.5f,   0f,     1.0f,0.0f,0.0f,
+
+            0f,     -0.25f, 0.0f,0.0f,1.0f,
+            0f,     0.25f,  1.0f,0.0f,0.0f
+    };
+
+
+    private int aColorLocation;
+    private int aPostionLocation;
+
+    GLRender1(Context context){
+        mContext = context;
+
+        vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT).order(
+                ByteOrder.nativeOrder()).asFloatBuffer();
+        vertexData.put(tableVertices);
+    }
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        String vertexShaderSource = com.roger.airhockey2.util.TextResourceReader.readTextFileFromResource(mContext,R.raw.simple_vertex_shader);
+        String fragmentShaderSource = com.roger.airhockey2.util.TextResourceReader.readTextFileFromResource(mContext,R.raw.simple_fragment_shader);
 
+        int vertexShader = com.roger.airhockey2.util.ShaderHelper.compileVertexShader(vertexShaderSource);
+        int fragmentShader = com.roger.airhockey2.util.ShaderHelper.compileFragmentShader(fragmentShaderSource);
+
+        program = com.roger.airhockey2.util.ShaderHelper.linkProgram(vertexShader,fragmentShader);
+
+        if(com.roger.airhockey2.util.LoggerConfig.ON){
+            com.roger.airhockey2.util.ShaderHelper.validateProgram(program);
+        }
+
+        GLES20.glUseProgram(program);
+        aColorLocation = GLES20.glGetAttribLocation(program,A_COLOR);
+        aPostionLocation = GLES20.glGetAttribLocation(program,A_POSITION);
+
+        vertexData.position(0);
+        GLES20.glVertexAttribPointer(aPostionLocation,POSITION_COMOPNENT_COUNT,GLES20.GL_FLOAT,false,STRIDE,vertexData);
+        GLES20.glEnableVertexAttribArray(aPostionLocation);
+
+        vertexData.position(POSITION_COMOPNENT_COUNT);
+        GLES20.glVertexAttribPointer(aColorLocation,COLOR_COMPONENT_COUNT,GLES20.GL_FLOAT,false,STRIDE,vertexData);
+        GLES20.glEnableVertexAttribArray(aColorLocation);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+        GLES20.glViewport(0, 0, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2);
 
     }
 }
--- Lesson1/app/src/main/java/com/example/Lesson1/MainActivity.java
+++ Lesson1/app/src/main/java/com/example/Lesson1/MainActivity.java
@@ -10,6 +10,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class MainActivity extends AppCompatActivity {
     private GLSurfaceView mGlSurfaceView = null;
+    private boolean mRenderSeted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +19,26 @@ public class MainActivity extends AppCompatActivity {
         mGlSurfaceView = new GLSurfaceView(this);
 
         mGlSurfaceView.setEGLContextClientVersion(2);
-        mGlSurfaceView.setRenderer(new GLRender1());
+        mGlSurfaceView.setRenderer(new GLRender1(this));
+        mRenderSeted = true;
 
         setContentView(mGlSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(mRenderSeted){
+            mGlSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mRenderSeted){
+            mGlSurfaceView.onPause();
+        }
     }
 }
