976dc22540e43c26c343d64eeb642ba676fa2e82
Robert <qingdong.cqd@alibaba-inc.com>
2020-01-27 20:09:44 +0800

    fix: 传入3维变量，shader中转换成4维变量;

--- Lesson2/app/src/main/res/raw/vertex_shader.glsl
+++ Lesson2/app/src/main/res/raw/vertex_shader.glsl
@@ -1,5 +1,5 @@
 attribute vec4 a_Position;
-attribute vec4 a_Color;
+attribute vec3 a_Color;
 
 
 varying vec4 v_Color;
@@ -8,5 +8,5 @@ void main() {
     gl_Position = a_Position;
     gl_PointSize = 20.0;
 
-    v_Color = a_Color;
+    v_Color = vec4(a_Color, 1.0f);
 }
