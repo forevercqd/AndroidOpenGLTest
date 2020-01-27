9467a0c6fa5eb11f824681c9427ee60cfb508a9d
Robert <qingdong.cqd@alibaba-inc.com>
2020-01-27 21:22:11 +0800

    feat: 增加GLRender3;

--- Lesson3/app/src/main/java/com/example/lesson3/MainActivity.java
+++ Lesson3/app/src/main/java/com/example/lesson3/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.lesson3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    GLSurfaceView mGLSurfaceView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setRenderer(new GLRender3(this));
+        setContentView(mGLSurfaceView);
     }
 }
